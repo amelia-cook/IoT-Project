9763c3afe88f2a0afaeecd230c130e34d70e01b2
Amelia Cook <ameliacook19@gmail.com>
2025-04-15 17:17:56 -0400

    printing, ip address, layout complete!

--- server.py
+++ server.py
@@ -1,17 +1,22 @@
-import flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def home()
-  return 'Hello from Raspberry Pi 5!'
+def home():
+    return 'Hello from Raspberry Pi 5!'
 
-@app.route('/data', methods=['POST'])
-def receive_data(): #fix this 
+@app.route('/calID', methods=['POST'])
+def receive_calID():
     data = request.json
     print(f"Received: {data}")
     return jsonify({"status": "success", "received": data}), 200
 
+@app.route('/getSticky', methods=['GET'])
+def send_sticky():
+    # data = request.json
+    # print(f"Received: {data}")
+    return jsonify({"status": "success", "received": "data"}), 200
+
 app.run(host="0.0.0.0", port=5000)
 
-#get raspberry pi ip address (in termianl) --> hostname -I 
